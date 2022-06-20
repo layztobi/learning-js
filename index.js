ecc026be0165a413057c6edd185024a10deef3fd
layztobi <64088185+layztobi@users.noreply.github.com>
2022-06-20 21:17:20 +0100

    commit numero uno

---
+++ index.js
@@ -0,0 +1,11 @@
+let user = {
+    Name:"OLARINRE, Samuel Oluwatobi",
+    Height: "188cm",
+    Country:"Nigeria"
+};
+alert("Name: "+user.Name);
+document.getElementById("name").innerHTML= "Name: "+ user.Name;
+alert("Height: "+user.Height);
+document.getElementById("height").innerHTML="Height: "+user.Height;
+alert("Country: "+user.Country);
+document.getElementById("country").innerHTML="Country: "+user.Country;
